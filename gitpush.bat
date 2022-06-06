823999cc710ed0f3a56193eafe0632abb1adca74
personqianduixue <3480814463@qq.com>
2022-06-06 21:49:15 +0800

    first commit

--- gitpush.bat
+++ gitpush.bat
@@ -1,6 +1,5 @@
-git init
 git add .
 git commit -m "first commit"
 git branch -M master
-git remote add origin https://github.com/personqianduixue/clash_node.git
 git push -u origin master
+python get_yaml_url.py
